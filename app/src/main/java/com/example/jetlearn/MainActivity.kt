f658b31b8812e6d5f61584c063f93ee3d9d570c4
Dawid <31769799+wybielak@users.noreply.github.com>
2024-02-02 14:42:19 +0100

    test

--- app/src/main/java/com/example/jetlearn/MainActivity.kt
+++ app/src/main/java/com/example/jetlearn/MainActivity.kt
@@ -25,3 +25,9 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
